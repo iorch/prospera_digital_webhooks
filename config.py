e5cd43f1d9d8abd3f56b35c9f9dd912b0cce4ea7
Jorge Martínez <j.martinezortega@gmail.com>
2015-08-06 17:31:48 +0000

    Adds ENV variables for tokens

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
     DATABASE_URI = None
-    RAPIDPRO_TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    RAPIDPRO_TOKEN = os.getenv('RAPIDPRO_TOKEN')
     
 class ProductionConfig(Config):
     DATABASE_URI = None
@@ -12,4 +14,4 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    COVERALL_TOKEN = 'AflqEfge180scOPSHqXvdecsGIJJdZejt' #previos token
+    COVERALL_TOKEN = os.getenv('COVERALLS_REPO_TOKEN')
