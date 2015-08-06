6efc9e97df64e7e0c0a21826b779ed9d91789ace
Jorge Martínez <j.martinezortega@gmail.com>
2015-08-06 17:31:06 +0000

    typo fix

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sudo locale-gen en_US.UTF-8
+export locale-gen en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANG=C.UTF-8
 
 cd /root/prospera_digital_webhooks
+mkdir ../logs
 pip install -r requirements.txt
 
 py.test --cov=prosperapp.py
