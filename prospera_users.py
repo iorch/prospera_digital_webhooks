17acdff20087b05e417536cb8ec562c0a8e7c9bc
iorch <j.martinezortega@gmail.com>
2015-08-17 22:30:31 -0500

    Adds database model for testing

---
+++ prospera_users.py
@@ -0,0 +1,16 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'dependencia'
+    dependenciaId = db.Column(db.BigInteger, primary_key=True)
+    nomDependencia = db.Column(db.String(255), index=True)
+    descdependencia = db.Column(db.String(255))
+    ambito_id = db.Column(db.BigInteger)
+    articulo_id = db.Column(db.BigInteger)
+    poder_id = db.Column(db.BigInteger)
+
+    def __init__(self,dependenciaId = 0):
+        self.dependenciaId = dependenciaId
+#end
