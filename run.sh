ddd8cdaaef614e7a207fb3827b075d934ad9b267
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2016-01-12 17:41:02 -0600

    script to run the containers

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env/ bash
+#!/usr/bin/env bash
 
 docker run -i -t \
 --name prodiwebhooks_mysql \
@@ -10,24 +10,6 @@ docker run \
 --name prodiwebhooks \
 --link prodiwebhooks_mysql:prodiwebhooks_mysql \
 -p 5001:5001 \
--e COVERALLS_REPO_TOKEN=$MY_COVERALL_TOKEN \
--e RAPIDPRO_TOKEN=$MY_RAPIDPRO_TOKEN \
-prospera_digital
-
-prodiwebhooks_mysql:
-  image: mysql
-  ports:
-    - "3306"
-  volumes:
-    - /prospera_digital/data:/var/lib/mysql
-  environment:
-    - MYSQL_ROOT_PASSWORD=helloNSA
-prodiwebhooks:
-  image: tutum.co/i0rch/prospera_digital_webhooks
-  links:
-      - prodiwebhooks_mysql
-    ports:
-      - "5001:5001"
-    environment:
-      - RAPIDPRO_TOKEN=bae15e0a8572ddb5f0e1c1fb8bb3733c84e3a746
-      - COVERALLS_REPO_TOKEN=bXnLtdX1UKRgQOMw9lXWJDgM9Sp7wRWoS
+-e COVERALLS_REPO_TOKEN=bXnLtdX1UKRgQOMw9lXWJDgM9Sp7wRWoS \
+-e RAPIDPRO_TOKEN=0000000a85000005f0e1c0000003733c84000000 \
+tutum.co/i0rch/prospera_digital_webhooks
