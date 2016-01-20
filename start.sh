24fe04c24953b768c8f7c5607bdcab06851f0189
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2016-01-20 17:59:49 -0600

    adds the line to start the queue

--- start.sh
+++ start.sh
@@ -3,6 +3,9 @@ export locale-gen en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANG=C.UTF-8
 
+cd /root/prospera_digital_webhooks/yowsup
+./start.sh &
+
 cd /root/prospera_digital_webhooks
 
 pip install coveralls
