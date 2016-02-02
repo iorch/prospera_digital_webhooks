a7556aa8f30307245c974eeb15b91af72e08e67a
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2016-02-02 13:36:39 -0600

    add instructions to setup database in test enviroment

--- .travis_testdb.sh
+++ .travis_testdb.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
-mysql -e "create database IF NOT EXISTS user;" -uroot
+mysql -e "create database IF NOT EXISTS user;" -uroot -p${PRODIWEBHOOKS_MYSQL_ENV_MYSQL_ROOT_PASSWORD}
 
 
-mysql -u root -e "USE user; \
-CREATE TABLE user (prosperaId bigint(20) NOT NULL, \
-clues varchar(255) NOT NULL,  nom_mun varchar(255) NOT NULL);"
+mysql -uroot -p${PRODIWEBHOOKS_MYSQL_ENV_MYSQL_ROOT_PASSWORD} -e "USE user;
+CREATE TABLE user (prosperaId bigint(20) NOT NULL,
+clues varchar(255) NOT NULL,  nom_mun varchar(255) NOT NULL); COMMIT;"
 
 
-mysql -u root -e "USE user; \
-INSERT INTO user VALUES \
-(1511110987654321,\
-'151111098',\
+mysql -uroot -p${PRODIWEBHOOKS_MYSQL_ENV_MYSQL_ROOT_PASSWORD} -e "USE user;
+INSERT INTO user VALUES
+(1511110987654321,
+'151111098',
 'GUADALAJARA');"
 
 echo "USE mysql;\n
 UPDATE user SET password=PASSWORD('$PRODIWEBHOOKS_MYSQL_ENV_MYSQL_ROOT_PASSWORD')
-WHERE user='root';\n FLUSH PRIVILEGES;\n" | mysql -u root
+WHERE user='root';\n FLUSH PRIVILEGES;\n" | mysql -uroot
