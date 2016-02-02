872544c003b509abca0f74237fa711c0890997fe
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2016-02-02 13:20:56 -0600

    adding capability to send messages. Sending a request for storing the message when it is received

--- yowsup/beanstalk-pd.py
+++ yowsup/beanstalk-pd.py
@@ -2,12 +2,15 @@ from pystalkd.Beanstalkd import Connection
 import datetime
 import json
 import requests
+import os
 
-#beanstalks = Connection(host="127.0.0.1", port=int(11300))
+beanstalks = Connection(host="127.0.0.1", port=int(11300))
 beanstalkr = Connection(host="127.0.0.1", port=int(11300))
-#beanstalks.use('whatsapp-send')
+beanstalks.use('whatsapp-send')
 beanstalkr.watch('whatsapp-receive')
 job = None
+message_received_url = os.environ.get('MESSAGE_RECEIVED_URL')
+
 while (job==None):
     job = beanstalkr.reserve(timeout=1)
     if (job!=None):
@@ -18,9 +21,11 @@ while (job==None):
         text = body["body"]
         to_send = {"from": address,
                    "text": "Mensaje: "+text+". Recibido a las "+str(body["timestamp"])+" desde el número: " + address,
-                   "date": timestamp}
+                   "date": date}
+        headers = {"Accept": "Application/json","Content-Type": "application/json"}
+        r = requests.post(message_received_url,
+                          data = json.dumps(to_send,ensure_ascii=False).encode('utf8'),headers=headers)
+        to_send["to"] = str(body["address"])
         message = json.dumps(to_send)
-        r = requests.post("http://localhost:5001/answer_rp",
-                          data = json.dumps(to_send,ensure_ascii=False).encode('utf8'))
-        #beanstalks.put(message)
+        beanstalks.put(message)
         job = None
