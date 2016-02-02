e716499ac2917d814c1291a1f468fc4951fa297d
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2016-02-02 13:33:36 -0600

    starting the instance

--- run.sh
+++ run.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-docker run -i -t \
+docker run \
 --name prodiwebhooks_mysql \
--v data_prodi/mysql:/var/lib/mysql \
 -e MYSQL_ROOT_PASSWORD=$MY_MYSQL_ROOT_PASSWORD \
 -p 3306:3306 -d mysql
 
+sleep 8
+mysql -uroot -h 192.168.99.100 -proot < test.sql
+mysql -uroot -h 192.168.99.100 -proot < messages.sql
+
 docker run \
---name prodiwebhooks \
+--name mxabierto-prospera_digital_webhooks \
 --link prodiwebhooks_mysql:prodiwebhooks_mysql \
+-e CELLPHONE=$MY_CELLPHONE \
+-e TEST_PHONE=$TEST_PHONE\
+-e PASSWORD=$MY_WHATSAPP_PASSWORD \
+-p 11300:11300 \
 -p 5001:5001 \
--e COVERALLS_REPO_TOKEN=bXnLtdX1UKRgQOMw9lXWJDgM9Sp7wRWoS \
--e RAPIDPRO_TOKEN=0000000a85000005f0e1c0000003733c84000000 \
-tutum.co/i0rch/prospera_digital_webhooks
+-e COVERALLS_REPO_TOKEN=$MY_COVERALLS_TOKEN \
+-e RAPIDPRO_TOKEN=$MY_RAPIDPRO_TOKEN \
+-e MESSAGE_RECEIVED_URL=$MY_MESSAGE_RECEIVED_URL \
+-e PD2_PHONENUM=$PD2_PHONENUM \
+-e UUID=$MYUUID
+mxabierto-prospera_digital_webhooks
