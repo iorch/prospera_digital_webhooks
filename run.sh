468018d080d3c3c56aa0d9205774d46a39c6d031
iorch <j.martinezortega@gmail.com>
2016-02-02 14:58:33 -0600

    correcting errors

--- run.sh
+++ run.sh
@@ -13,13 +13,13 @@ docker run \
 --name mxabierto-prospera_digital_webhooks \
 --link prodiwebhooks_mysql:prodiwebhooks_mysql \
 -e CELLPHONE=$MY_CELLPHONE \
--e TEST_PHONE=$TEST_PHONE\
+-e TEST_PHONE=$TEST_PHONE \
 -e PASSWORD=$MY_WHATSAPP_PASSWORD \
 -p 11300:11300 \
 -p 5001:5001 \
 -e COVERALLS_REPO_TOKEN=$MY_COVERALLS_TOKEN \
 -e RAPIDPRO_TOKEN=$MY_RAPIDPRO_TOKEN \
 -e MESSAGE_RECEIVED_URL=$MY_MESSAGE_RECEIVED_URL \
--e PD2_PHONENUM=$PD2_PHONENUM \
--e UUID=$MYUUID
+-e PD2_PHONENUM="$PD2_PHONENUM" \
+-e UUID=$MYUUID \
 mxabierto-prospera_digital_webhooks
