6047fc8333824682d4e4b9bd8a42d781f9a62106
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2016-02-02 13:09:56 -0600

    simplifies file

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config(object):
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     if os.getenv('DEBUG_MODE'):
         DEBUG = os.getenv('DEBUG_MODE')
     else:
@@ -11,26 +12,16 @@ class Config(object):
         TESTING = False
     DATABASE_URI = None
     RAPIDPRO_TOKEN = os.getenv('RAPIDPRO_TOKEN')
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:' + \
-    #    os.getenv('PRODIWEBHOOKS_MYSQL_ENV_MYSQL_ROOT_PASSWORD') + \
-    #    '@' + os.getenv('PRODIWEBHOOKS_MYSQL_PORT_3306_TCP_ADDR') + \
-    #    '/dependencia'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:' + \
+        os.getenv('PRODIWEBHOOKS_MYSQL_ENV_MYSQL_ROOT_PASSWORD') + \
+        '@' + os.getenv('PRODIWEBHOOKS_MYSQL_PORT_3306_TCP_ADDR') + \
+        '/pddb'
 
 class ProductionConfig(Config):
     PRODUCTION = True
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:' + \
-        'root' + \
-        '@' + 'localhost' + \
-        '/user'
-
 
 class TestingConfig(Config):
     TESTING = True
-    COVERALL_TOKEN = os.getenv('COVERALLS_REPO_TOKEN')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:' + \
-        os.getenv('PRODIWEBHOOKS_MYSQL_ENV_MYSQL_ROOT_PASSWORD') + \
-        '@' + os.getenv('PRODIWEBHOOKS_MYSQL_PORT_3306_TCP_ADDR') + \
-        '/user'
