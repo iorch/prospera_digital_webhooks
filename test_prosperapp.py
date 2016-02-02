19157be585bec3b513bff10c3605634b38c8cbb6
Jorge Martinez Ortega <j.martinezortega@gmail.com>
2016-02-02 13:32:58 -0600

    adds some tests

--- test_prosperapp.py
+++ test_prosperapp.py
@@ -5,7 +5,7 @@ import pytest
 import prosperapp
 import json
 import random
-
+import os
 
 @pytest.fixture
 def app():
@@ -36,13 +36,13 @@ def test_hello_post(test_client):
 
 def test_translate_age(test_client):
     response = test_client.post('/translate_age', data=dict(
-        phone='(206) 555-0104'), follow_redirects=True)
+        phone=os.environ.get('PD2_PHONENUM'), follow_redirects=True)
     assert json.loads(response.data)['birthdate'] == '1992-10-30'
 
 
 def test_query(test_client):
     response = test_client.get('/query?prosperaId=1511110987654321&variables=clues&variables=nom_mun', )
-    assert json.loads(response.data.decode("utf-8"))['nom_mun'] == 'GUADALAJARA'
+    assert json.loads(response.data.decode("utf-8"))['nom_mun'] == 'Guadalajara'
 
 def test_generate_random_get(test_client):
     randomn=random.sample([4,6,8],1)[0]
@@ -51,13 +51,23 @@ def test_generate_random_get(test_client):
 
 def test_get_info_post(test_client):
     response = test_client.post('/get_info', data=dict(
-        uuid='c5c0814e-59e9-4327-95ec-073c0aa74936',
+        uuid=os.environ.get('UUID'),
         prosperaId='1511110987654321',
-        pd2_phonenum='(206) 555-0104'), follow_redirects=True)
+        pd2_phonenum=os.environ.get('PD2_PHONENUM'), follow_redirects=True))
     assert response.status == "200 OK"
 
 
 def test_get_info(test_client):
-    response = test_client.get('/get_info?uuid=c5c0814e-59e9-4327-95ec-073c0aa74936&pd2_phonenum=(206) 555-0104&prosperaId=1511110987654321', )
+    response = test_client.get(
+        '/get_info?uuid='+os.environ.get('UUID')+'&pd2_phonenum='+os.environ.get('PD2_PHONENUM')+'&prosperaId=1511110987654321', )
     assert response.status == "200 OK"
+
+#def test_pdys(test_client):
+#    from_cel = os.environ.get('CELLPHONE')
+#    text = 'test'
+#    to_cel = os.environ.get('TEST_PHONE')
+#    url = '/pdys?from={0}&text={1}&to={2}'.format(from_cel,text,to_cel)
+#    response = test_client.post(url, follow_redirects=True)
+#    print(json.dumps(json.loads(response.data.decode("utf-8"))))
+#    assert response.status == "200 OK"
 
